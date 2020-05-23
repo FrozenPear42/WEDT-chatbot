9244969810ee16d41f8506bea92c25ed7277cc4e
Jakub Gajownik <gajus123@gmail.com>
2020-05-23 19:13:22 +0200

    Remove attention plotting

--- sp2en.py
+++ sp2en.py
@@ -323,49 +323,27 @@ def evaluate(sentence):
   dec_input = tf.expand_dims([targ_lang.word_index['<start>']], 0)
 
   for t in range(max_length_targ):
-    predictions, dec_hidden_h, dec_hidden_c, attention_weights = decoder(dec_input,
+    predictions, dec_hidden_h, dec_hidden_c, _ = decoder(dec_input,
                                                                          dec_hidden_h,
                                                                          enc_out)
-
-    # storing the attention weights to plot later on
-    attention_weights = tf.reshape(attention_weights, (-1, ))
-    attention_plot[t] = attention_weights.numpy()
 
     predicted_id = tf.argmax(predictions[0]).numpy()
 
     result += targ_lang.index_word[predicted_id] + ' '
 
     if targ_lang.index_word[predicted_id] == '<end>':
-      return result, sentence, attention_plot
+      return result, sentence
 
     # the predicted ID is fed back into the model
     dec_input = tf.expand_dims([predicted_id], 0)
 
-  return result, sentence, attention_plot
-
-# function for plotting the attention weights
-def plot_attention(attention, sentence, predicted_sentence):
-  fig = plt.figure(figsize=(10,10))
-  ax = fig.add_subplot(1, 1, 1)
-  ax.matshow(attention, cmap='viridis')
-
-  fontdict = {'fontsize': 14}
-
-  ax.set_xticklabels([''] + sentence, fontdict=fontdict, rotation=90)
-  ax.set_yticklabels([''] + predicted_sentence, fontdict=fontdict)
-
-  ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-  ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-  plt.show()
+  return result, sentence
 
 def translate(sentence):
-  result, sentence, attention_plot = evaluate(sentence)
+  result, sentence = evaluate(sentence)
 
   print('Input: %s' % (sentence))
   print('Predicted translation: {}'.format(result))
-#   attention_plot = attention_plot[:len(result.split(' ')), :len(sentence.split(' '))]
-#   plot_attention(attention_plot, sentence.split(' '), result.split(' '))
 
 # restoring the latest checkpoint in checkpoint_dir
 # checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
